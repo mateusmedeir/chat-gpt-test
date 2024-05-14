97b9788abb1947e4fc00f8667198a34f49bae1f5
mateus <mateusmlopestr@gmail.com>
2024-05-14 20:12:07 -0300

    refactor: :recycle: enhance home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -75,7 +75,20 @@ export default function Home() {
     const chat = model.startChat({
       generationConfig,
       safetySettings,
-      history,
+      history: [
+        {
+          role: "user",
+          parts: [{ 
+            text: 
+            "Você é um especialista em marketing de mercado exterior, prestando um serviço para uma empresa de fora do Brasil que deseja exportar seu produto. Você precisa responder perguntas estruturadas para o seu cliente, como se fosse um relatório. A lista de perguntas segue a ordem abaixo numerada e o que está entre parênteses precisa ser respondido obrigatoriamente mas não necessariamente apenas isso\nSituação econômica atual do Brasil (dados de PIB, contexto global e territorial)\n2.Expectativas da economia para um período de um ano, dividido por trimestres(dados de inflação, expectativas para o futuro global e territorial)\nNecessário também que possua a fonte para cada resposta à pergunta\nResponda tudo isso numa estrutura discorrida, como se estivesse conversando, de forma formal, comigo"
+           }],
+        },
+        {
+          role: "model",
+          parts: [{ text: "Perfeito, estou a sua disposição." }],
+        },
+        ...history
+      ],
     });
 
     const result = await chat.sendMessage(prompt);
@@ -97,14 +110,14 @@ export default function Home() {
 
   return (
     <div className="flex flex-col min-h-screen">
-      <div className="sticky top-0 z-10 p-4 border-b bg-background">
+      <div className="sticky top-0 z-10 p-4 bg-background/">
         <h1 className="text-xl font-semibold text-center">Market Monitor</h1>
       </div>
-      <div className="flex-1 flex w-full h-full max-w-[500px] mx-auto pt-6">
-        <div className="w-full min-h-full relative flex flex-col flex-1 justify-between gap-12">
+      <div className="container flex flex-1 pt-6">
+        <div className="w-full min-h-full relative flex flex-col justify-between gap-12">
           <div className="flex flex-col gap-12 overflow-y-auto">
             {text.map(({ user, model }, index) => (
-              <div key={index} className="flex flex-col gap-8 text-sm">
+              <div key={index} className="flex flex-col gap-6 text-sm">
                 <div className="flex flex-col gap-0.5 text-gray-400">
                   <p className="font-bold">Você</p>
                   <p>{user.text}</p>
@@ -119,7 +132,7 @@ export default function Home() {
           <div className="sticky bottom-0 pb-6 bg-background">
             <form
               onSubmit={onSubmit}
-              className="flex px-3 items-center overflow-hidden w-full border rounded-lg bg-background focus-within:ring-1 focus-within:ring-ring"
+              className="flex px-3 items-center overflow-hidden w-full border rounded-full bg-background focus-within:ring-1 focus-within:ring-ring"
             >
               <Input
                 name="prompt"
@@ -129,7 +142,7 @@ export default function Home() {
                 placeholder="Digite aqui sua mensagem"
                 className="resize-none h-[52px] p-0 border-0 shadow-none focus-visible:ring-0 focus-visible:ring-offset-0"
               />
-              <Button type="submit" size="icon" disabled={!input || isLoading}>
+              <Button className="bg-primary rounded-full" type="submit" size="icon" disabled={!input || isLoading}>
                 {isLoading ? (
                   <LoaderCircle size={18} className="animate-spin" />
                 ) : (
